f412c0ff7ccc0b64b2ae591fa0486ce124d3e2a6
Royer <retro.rever@gmail.com>
2016-12-21 16:22:02 -0600

    primeros avances de arquitectura del proyecto

---
+++ gcSystem-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gcSystemFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gcSystemFrontendApp
+ */
+angular.module('gcSystemFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
